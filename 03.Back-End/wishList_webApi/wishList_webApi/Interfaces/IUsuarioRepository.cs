9ce2276a5a8d5e23bf7d1d1ad6694f1857b2e266
Erika Leite <erika.l3it3@gmail.com>
2021-05-27 12:37:37 -0300

    back-end 75% ok

---
+++ 03.Back-End/wishList_webApi/wishList_webApi/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using wishList_webApi.Domains;
+
+namespace wishList_webApi.Interfaces
+{
+    interface IUsuarioRepository
+    {
+        List<Usuario> Listar();
+
+        
+        Usuario BuscarPorId(int id);
+
+        
+        void Cadastrar(Usuario novoUsuario);
+
+        
+        void Atualizar(int id, Usuario usuarioAtualizado);
+
+    
+        void Deletar(int id);
+
+    }
+}
